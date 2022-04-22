63c640c7924030f1b84c1af2da4ab043d6522b6c
damnow777 <damiannowak32.dn@gmail.com>
2022-04-22 12:08:22 +0200

    Dodaj pliki projektów.

---
+++ Form1.Designer.cs
@@ -0,0 +1,157 @@
+﻿namespace ImageApp2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Wymagana zmienna projektanta.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Wyczyść wszystkie używane zasoby.
+        /// </summary>
+        /// <param name="disposing">prawda, jeżeli zarządzane zasoby powinny zostać zlikwidowane; Fałsz w przeciwnym wypadku.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Kod generowany przez Projektanta formularzy systemu Windows
+
+        /// <summary>
+        /// Metoda wymagana do obsługi projektanta — nie należy modyfikować
+        /// jej zawartości w edytorze kodu.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.Load = new System.Windows.Forms.Button();
+            this.Clear = new System.Windows.Forms.Button();
+            this.Exit = new System.Windows.Forms.Button();
+            this.Save = new System.Windows.Forms.Button();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(24)))), ((int)(((byte)(30)))), ((int)(((byte)(54)))));
+            this.panel1.Controls.Add(this.Save);
+            this.panel1.Controls.Add(this.Exit);
+            this.panel1.Controls.Add(this.Clear);
+            this.panel1.Controls.Add(this.Load);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Left;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(200, 510);
+            this.panel1.TabIndex = 0;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.pictureBox1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(56)))), ((int)(((byte)(61)))), ((int)(((byte)(83)))));
+            this.pictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.pictureBox1.Location = new System.Drawing.Point(218, 0);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(718, 510);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Load
+            // 
+            this.Load.FlatAppearance.BorderSize = 0;
+            this.Load.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(58)))), ((int)(((byte)(50)))), ((int)(((byte)(78)))));
+            this.Load.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.Load.Font = new System.Drawing.Font("Comic Sans MS", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.Load.ForeColor = System.Drawing.Color.White;
+            this.Load.Location = new System.Drawing.Point(3, 27);
+            this.Load.Name = "Load";
+            this.Load.Size = new System.Drawing.Size(194, 38);
+            this.Load.TabIndex = 0;
+            this.Load.Text = "Wczytaj";
+            this.Load.UseVisualStyleBackColor = true;
+            this.Load.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Clear
+            // 
+            this.Clear.FlatAppearance.BorderSize = 0;
+            this.Clear.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(58)))), ((int)(((byte)(50)))), ((int)(((byte)(78)))));
+            this.Clear.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.Clear.Font = new System.Drawing.Font("Comic Sans MS", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.Clear.ForeColor = System.Drawing.Color.White;
+            this.Clear.Location = new System.Drawing.Point(6, 115);
+            this.Clear.Name = "Clear";
+            this.Clear.Size = new System.Drawing.Size(194, 38);
+            this.Clear.TabIndex = 1;
+            this.Clear.Text = "Wyczyść";
+            this.Clear.UseVisualStyleBackColor = true;
+            this.Clear.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Exit
+            // 
+            this.Exit.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.Exit.FlatAppearance.BorderSize = 0;
+            this.Exit.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(58)))), ((int)(((byte)(50)))), ((int)(((byte)(78)))));
+            this.Exit.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.Exit.Font = new System.Drawing.Font("Comic Sans MS", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.Exit.ForeColor = System.Drawing.Color.White;
+            this.Exit.Location = new System.Drawing.Point(3, 433);
+            this.Exit.Name = "Exit";
+            this.Exit.Size = new System.Drawing.Size(194, 38);
+            this.Exit.TabIndex = 2;
+            this.Exit.Text = "Zakończ";
+            this.Exit.UseVisualStyleBackColor = true;
+            this.Exit.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // Save
+            // 
+            this.Save.FlatAppearance.BorderSize = 0;
+            this.Save.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(58)))), ((int)(((byte)(50)))), ((int)(((byte)(78)))));
+            this.Save.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.Save.Font = new System.Drawing.Font("Comic Sans MS", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.Save.ForeColor = System.Drawing.Color.White;
+            this.Save.Location = new System.Drawing.Point(3, 71);
+            this.Save.Name = "Save";
+            this.Save.Size = new System.Drawing.Size(194, 38);
+            this.Save.TabIndex = 3;
+            this.Save.Text = "Zapisz";
+            this.Save.UseVisualStyleBackColor = true;
+            this.Save.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(934, 510);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.panel1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Przetwarzanie obrazu";
+            this.panel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button Load;
+        private System.Windows.Forms.Button Clear;
+        private System.Windows.Forms.Button Exit;
+        private System.Windows.Forms.Button Save;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ImageApp2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        // LOAD FILE
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                OpenFileDialog fileDialog = new OpenFileDialog();
+                fileDialog.Title = "Wybierz obraz";
+                fileDialog.Filter = "jpg files (*.jpg) |*.jpg| PNG files (*.png)|*.png| All Files (*.*) |*.*";
+
+                if (fileDialog.ShowDialog() == DialogResult.OK)
+                {
+                    Bitmap image = new Bitmap(fileDialog.FileName);
+                    //int x, y;
+
+                    //for (x = 0; x < image.Width; x++)
+                    //{
+                    //    for (y = 0; y < image.Height; y++)
+                    //    {
+                    //        Color pixelColor = image.GetPixel(x, y);
+                    //        Color newColor = Color.FromArgb(pixelColor.G, 0, 0);
+                    //        image.SetPixel(x, y, newColor);
+                    //    }
+                    //}
+                    pictureBox1.Image = image;
+                }
+            }
+            catch (Exception)
+            {
+
+                MessageBox.Show("Błąd wczytywania obrazu", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        // CLEAR PICTUREBOX
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (pictureBox1.Image != null)
+                ImageClear();
+            else
+                MessageBox.Show("Brak wczytanego obrazu", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+
+        }
+
+        // CLEAR PICTUREBOX METHOD
+        private void ImageClear()
+        {
+            pictureBox1.CancelAsync();
+            pictureBox1.Image.Dispose();
+            pictureBox1.Image = null;
+        }
+
+        // SAVE IMAGE METHOD
+        private void SaveImage()
+        {
+            try
+            {
+                SaveFileDialog fileDialog = new SaveFileDialog();
+                fileDialog.Title = "Zapisz obraz";
+                fileDialog.Filter = "jpg files (*.jpg) |*.jpg| PNG files (*.png)|*.png| All Files (*.*) |*.*";
+
+                if (fileDialog.ShowDialog() == DialogResult.OK)
+                {
+                    if (fileDialog.FileName != "")
+                    {
+                        pictureBox1.Image.Save(fileDialog.FileName);
+                    }
+                }
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("Błąd zapisu obrazu", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+
+            }
+        }
+
+        // EXIT APP
+        private void button3_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        // SAVE IMAGE
+        private void button4_Click(object sender, EventArgs e)
+        {
+            if(pictureBox1.Image != null)
+                SaveImage();
+            else
+                MessageBox.Show("Brak obrazu do zapisu", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+        }
+    }
+}
